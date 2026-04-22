9b329383020263f5f0da203981fa17a6a1898835
brazilgustavo <brazil.gustavo@gmail.com>
2026-04-22 18:27:09 -0300

    chore: remove tracked target files

--- backend/services/product-service/target/classes/db/migration/V3__fix_numeric_columns.sql
+++
@@ -1,3 +0,0 @@
--- Fix numeric column types from NUMERIC to DOUBLE PRECISION
-ALTER TABLE produtos_base
-    ALTER COLUMN peso_volume TYPE DOUBLE PRECISION USING peso_volume::DOUBLE PRECISION;
--- backend/services/supermarket-service/target/classes/db/migration/V2__add_whitelabel_fields.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE supermercados
-ADD COLUMN url_logomarca VARCHAR(500),
-ADD COLUMN cor_primaria_hex VARCHAR(7),
-ADD COLUMN cor_secundaria_hex VARCHAR(7);
